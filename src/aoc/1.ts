4fd300c208e98057de5af11bd3a9709dc854f610
Roberto Sero <>
2021-12-03 21:57:50 +0100

    1

--- src/aoc/1.ts
+++ src/aoc/1.ts
@@ -1,1 +1,19 @@
-export const a = 1;
+import { pipeline } from 'stream';
+import * as readline from 'readline'
+const log = console.error.bind(console);
+
+export async function d1(input: NodeJS.ReadableStream) {
+  const rl = readline.createInterface({
+    input,
+  });
+  let curr, prev;
+  let n = 0;
+  for await (const line of rl) {
+    curr = +line;
+    if (prev !== undefined && curr > prev) {
+      n++;
+    }
+    prev = curr;
+  }
+  return n;
+}
--- src/cmd/main.ts
+++ src/cmd/main.ts
@@ -12,12 +12,13 @@ const log = console.error.bind(console);
 
 async function main() {
   const [ n ] = process.argv.slice(2);
-  
+  const res = await aoc[`d${n}`](process.stdin);
+  log(res)
 }
 
 main()
   .then(() => process.exit(0))
   .catch((err) => {
-    console.log(err.toJSON());
+    console.log(err);
     process.exit(1);
   });
